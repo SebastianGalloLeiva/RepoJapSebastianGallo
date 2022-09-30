f5f71c2f55b8ea227e486b4d8b09cfa8004d16bb
Sebastian Gallo <elgalo13@gmail.com>
2022-09-29 23:16:46 -0300

    Cuarta Entrega

--- js/product-info.js
+++ js/product-info.js
@@ -8,17 +8,28 @@ let userScore = undefined;
 let user = localStorage.getItem("email")
 let comentarioNro = 1;
 let usercomment = ""
+let comentariosdelapeople = []
 
 
-
+function setProdID(id) {
+    localStorage.setItem("prodID", id);
+    window.location = "product-info.html"
+}
 
 function showProduct(){
     let htmlContentToAppend= "";
     let images = product.images
     let image = ""
+    image = `<div class="carousel-item active">
+    <img src="${product.images[0]}" class="d-block w-100">
+  </div>`;
+    images.shift()
     for (let i in images) {
-        image += `<img src="${product.images[i]}" class="col-3"></img>`
-       }
+        image += `<div class="carousel-item">
+        <img src="${images[i]}" class="d-block w-100">
+      </div>`
+    }
+       
     htmlContentToAppend += `
          <h1> ${product.name} </h1>
          <hr>
@@ -35,18 +46,24 @@ function showProduct(){
          <p2>${product.soldCount} </p2> 
          <br>
          <h2> Imagenes ilustrativas </h2>
-         <div class="row m-3">
-            ${image}
-            </div>
+         
+    
+  </div>
+</div>
          `
          document.getElementById("info-producto").innerHTML = htmlContentToAppend;
+         contenidoParaAgregar = `
+         ${image}
+         `
+         document.getElementById("imagenes-carousel").innerHTML += contenidoParaAgregar
 }
 
 function showComments(){
     let htmlContentToAppend ="";
     let comments = currentProductComments;
     let rating = ""
-    if (comments.length == 0){ htmlContentToAppend = ` <p>No hay comentarios, sé el primero en comentar! </p>`
+    if ((comments.length == 0 ) && (!localStorage.getItem(`commentario${id}`))){ htmlContentToAppend = ` <p>No hay comentarios, sé el primero en comentar! </p>`
+    // console.log((localStorage.getItem(`commentario${id}`)))
     document.getElementById("user-comment").innerHTML = htmlContentToAppend;
 };
 
@@ -77,40 +94,91 @@ function showComments(){
         `
         document.getElementById("user-comment").innerHTML = htmlContentToAppend;
         rating = "";
-    }
-}
+}};
+
+function showNewComments(){
+    let htmlContentToAppend = "";
+    let newrating = "";
+    // htmlContentToAppend = JSON.parse(localStorage.getItem(`commentario${id}`))
+    let newcomments = JSON.parse(localStorage.getItem(`commentario${id}`))
+    for (let i = 0; i < newcomments.length; i++) {
+        let newcomment = newcomments[i];        
+        let scores = newcomment.score;
+    for (let i = 0; i <= scores; i++) {
+        newrating += `<span class="fa fa-star checked"></span>`
+        } 
+    for (let i = scores; i<5; i++) {
+        newrating += `<span class="fa fa-star "></span>`
+    }   
+
+        htmlContentToAppend =`
+        <div class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1"><strong>${newcomment.User}</strong></h4>
+                            <p class="mb-1"> ${newcomment.fecha} </p>
+                           <div class="col-3" ${newrating} </div>
+                        </div>
+                        <p class="mb-1">${newcomment.commentario}</p>
+                    </div>
+                </div>
+            </div>
+        `
+    document.getElementById("user-comment").innerHTML += htmlContentToAppend;
+    newrating = "";
+}};
 
 function saveComment(comentarioUsuario, puntaje){
-    if (!localStorage.getItem("contador")){
-    localStorage.setItem("contador", comentarioNro)};
-    contador = localStorage.getItem("contador")
-    contador ++
-    let commentario = comentarioUsuario;
-    let score = puntaje;
-    comentarioNro ++;  
+    if (localStorage.getItem(`commentario${id}`)){
+    comentariosdelapeople = JSON.parse(localStorage.getItem(`commentario${id}`))}
+    // let commentario = comentarioUsuario;
+    // let score = puntaje;
     let now = new Date();
-    let dateTime = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()} `;
-        dateTime += `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    let ano = now.getFullYear();
+    let mes = ("0" + (now.getMonth()+1)).slice(-2);
+    let dia = ("0" + now.getDate()).slice(-2);
+    let horas = ("0" + now.getHours()).slice(-2);
+    let minutos = ("0" + now.getMinutes()).slice(-2);
+    let segundos = ("0" + now.getSeconds()).slice(-2);
+    let dateTime = `${ano}-${mes}-${dia} `;
+        dateTime += `${horas}:${minutos}:${segundos}`;
         usercomment = { User: user,
                         score: userScore,
                         commentario: comentario,
                         productoid: id,
-                        fecha: dateTime,
-                        
+                        fecha: dateTime,           
         }
+        
+        comentariosdelapeople.push(usercomment)
 
-    localStorage.setItem(`commentario${contador + id}`, JSON.stringify(usercomment))
-    localStorage.setItem('contador', contador ++)
+    localStorage.setItem(`commentario${id}`, JSON.stringify(comentariosdelapeople))
+   }
 
-}
-
+function showRelatedProducts(){
+    let htmlContentToAppend= "";
+    for (let i = 0; i < relatedProducts.length; i++) {
+        let relatedProduct = relatedProducts[i];
+    
+    htmlContentToAppend += `
+    <div onclick="setProdID(${relatedProduct.id})" class="w-25 col-sm list-group-item list-group-item-action cursor-active">
+    
+    <img src="${relatedProduct.image}" class="img-fluid"></img>
+            ${relatedProduct.name}
+            </div>
+         `
+         document.getElementById("related-products").innerHTML = htmlContentToAppend;
+}}
 
 
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_URL + id + ".json").then(function(resultObj){
         if (resultObj.status === "ok"){
             product = resultObj.data
-            showProduct();            
+            relatedProducts = product.relatedProducts
+            showProduct();
+            showRelatedProducts()            
         }
     })
     
@@ -121,7 +189,6 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_COMMENTS_URL + id + ".json").then(function(resultComm){
         if (resultComm.status === "ok"){
             currentProductComments = resultComm.data
-            // console.log(currentProductComments);
             showComments(currentProductComments);
         }
     })
@@ -130,12 +197,25 @@ document.addEventListener("DOMContentLoaded", function(e){
 document.getElementById("enviar-comentario").addEventListener("click", function(){
         comentario = document.getElementById("user-made-comment").value;
         userScore = document.getElementById("user-rating").value;
+        console.log(userScore)
+        let seCumple = true;
+    if (comentario =="") {
+        seCumple = false;
+        alert("Ingresar comentario por favor.");
+    }
+    if (userScore =="Tu puntuacion"){
+        seCumple = false;
+        alert("Ingresar puntuacion.");
+    }
+    if (seCumple) {
         saveComment(comentario, userScore);
-        
-        
+        }
 })
 
-
-
-
-
+document.addEventListener("DOMContentLoaded", function(e){
+    if (localStorage.getItem(`commentario${id}`)){
+        getJSONData(PRODUCT_INFO_URL + id + ".json").then(function(resultObj){
+            if (resultObj.status === "ok"){
+        showNewComments();
+    }
+})}});
--- js/showuser.js
+++ js/showuser.js
@@ -1,11 +1,25 @@
 function showUser(){
+    if(localStorage.getItem("email")) {
     let user = localStorage.getItem("email")
     let htmlContentToAppend = ""
         htmlContentToAppend += `
-            <p class="nav-link">${user}</p> 
-           `          
+        <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    ${user}
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="cart.html">Mi carrito</a></li>
+    <li><a class="dropdown-item" href="my-profile.html">Mi perfil</a></li>
+    <li><a class="dropdown-item" href="index.html"  >Cerrar sesión</a></li>
+  </ul>
+</div>
+           `      
+         try {  
         document.getElementsByClassName('nav-item')[3].innerHTML += htmlContentToAppend;
-     }
+    } catch (error){
+        window.localStorage.removeItem("email")
+    }
+    }}
     
     
 document.addEventListener("DOMContentLoaded", function(){

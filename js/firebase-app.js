b9abb357bca8756b3386be579f3be01f303312c0
Sebastian Gallo <elgalo13@gmail.com>
2022-11-03 22:51:03 -0300

    Septima Entrega

--- js/firebase-app.js
+++ js/firebase-app.js
@@ -3,3 +3,28 @@ import {initializeApp} from "https://www.gstatic.com/firebasejs/9.11.0/firebase-
 import { GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-auth.js";
 
 const provider = new GoogleAuthProvider();
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-auth.js";
+
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+  document.getElementById("boton-google").addEventListener("DOMContentLoaded", function (){
+      signInWithPopup()
+  })

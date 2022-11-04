bfb0f58c5bc5f4f5dcd5fc3de448d4ab34a69cbe
Sebastian Gallo <elgalo13@gmail.com>
2022-11-03 23:46:16 -0300

    Septima Entrega

--- js/firebase-app.js
+++ js/firebase-app.js
@@ -1,5 +1,20 @@
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDQnF8_PvMcMFBJSqhHBjnZ6EGaaX-G9vE",
+    authDomain: "loginauthjap.firebaseapp.com",
+    projectId: "loginauthjap",
+    storageBucket: "loginauthjap.appspot.com",
+    messagingSenderId: "892651063501",
+    appId: "1:892651063501:web:b2f5e6ae4883d2d6d6c184"
+  };
+  
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 import { GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-auth.js";
 
 const provider = new GoogleAuthProvider();
@@ -24,6 +39,8 @@ signInWithPopup(auth, provider)
     const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
   });
+
+  
 
   document.getElementById("boton-google").addEventListener("DOMContentLoaded", function (){
       signInWithPopup()
